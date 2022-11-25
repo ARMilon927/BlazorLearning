db20dc3157ba7e8d0608f21bc6e3ed19162a07c1
Milon927 <a.r.milon927@gmail.com>
2022-11-25 12:05:49 +0600

    split component 2 ways

---
+++ BlazorServer/Pages/FetchData.razor.cs
@@ -0,0 +1,20 @@
+﻿using BlazorServer.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.Pages
+{
+    public partial class FetchData
+    {
+       
+        public WeatherForecastService ForecastService = new WeatherForecastService();
+
+        private WeatherForecast[]? forecasts;
+
+       
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
